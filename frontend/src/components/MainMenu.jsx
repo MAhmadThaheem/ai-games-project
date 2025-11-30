f4efd599a44ac8f4f551b0ada89c7029be0460c7
MAhmadThaheem <muhammadahmad28423@gmail.com>
2025-12-01 00:51:15 +0500

    fixed bugs

--- frontend/src/components/MainMenu.jsx
+++ frontend/src/components/MainMenu.jsx
@@ -6,6 +6,7 @@ const MainMenu = () => {
     <div className="min-h-screen bg-[#0a0a0c] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-900/20 via-gray-900 to-black text-white relative">
       
       {/* 1. The Navigation Bar (Search, Links, Music) */}
+
       <Navbar />
 
       {/* 2. The Hero Section (Title, Play Button) */}
